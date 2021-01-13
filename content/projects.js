ab03042093264457962aef0e96e8fa5f0305f32f
misterblack0 <misterblack0@protonmail.com>
2021-01-13 21:45:48 +0200

    projects section

---
+++ content/projects.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        id: 1,
+        title: "Test Project",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa sapien faucibus et molestie ac feugiat sed lectus. Interdum varius sit amet mattis vulputate enim nulla. Quis hendrerit dolor magna eget.",
+        cover: "https://i.postimg.cc/52MGRBpw/1.png",
+        extern: "https://",
+        github: "https://github.com/",
+        tech: ["React", "TMDB API", "React-Router"]
+    },
+    {
+        id: 2,
+        title: "Portfolio",
+        description:
+            "Responsive website built with ReactJS. The site content is passed in as JSON data, auto-generating components for easy maintenance.",
+        cover: "https://i.postimg.cc/52MGRBpw/1.png",
+        extern: "https://mariusciocoiu.com",
+        github: "https://github.com/",
+        tech: ["HTML5", "CSS3", "Sass", "Javascript", "ReactJS"]
+    }
+];
+
+export default projects;
