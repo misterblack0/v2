eeb175210f136dd8f45c6a6ee8973e8d153fcc13
misterblack0 <misterblack0@protonmail.com>
2021-01-14 18:53:13 +0200

    test projects update

--- content/projects.js
+++ content/projects.js
@@ -3,11 +3,11 @@ const projects = [
         id: 1,
         title: "Test Project",
         description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa sapien faucibus et molestie ac feugiat sed lectus. Interdum varius sit amet mattis vulputate enim nulla. Quis hendrerit dolor magna eget.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ipsum incididunt ut consectetur adipiscing labore et dolore magna aliqua.",
         cover: "/cover.jpg",
         extern: "https://",
         github: "https://github.com/",
-        tech: ["React", "TMDB API", "React-Router"]
+        tech: ["Next.js", "TMDB API"]
     },
     {
         id: 2,
@@ -17,7 +17,7 @@ const projects = [
         cover: "/cover.jpg",
         extern: "https://mariusciocoiu.com",
         github: "https://github.com/",
-        tech: ["Sass", "ReactJS"]
+        tech: ["Sass", "React"]
     }
 ];
 
