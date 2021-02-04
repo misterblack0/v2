fd3ca0e4a2fefe4142fba04fbeeb4516c7f3c1b7
misterblack0 <47751056+misterblack0@users.noreply.github.com>
2021-02-04 18:39:40 +0200

    footer visual bug fix

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,30 +1,109 @@
 import React from "react";
 import styled from "styled-components";
 import { email } from "../../config";
-import Image from "next/image";
 
-const Wrapper = styled.div`
-    background: linear-gradient(to bottom right, #c04de2, #340c7f);
+const StyledFooterSection = styled.footer`
     position: relative;
-    height: 95vh;
+    height: 516px;
+    padding: 192px 0 0;
+    background-image: linear-gradient(to bottom right, #c04de2, #340c7f);
+
+    @media (min-width: 768px) {
+        height: 628px;
+        padding: 280px 0 0;
+    }
+    @media (min-width: 1200px) {
+        height: 900px;
+        padding: 436px 0 0;
+    }
 `;
 
-const BgPattern = styled(Image)`
-    z-index: 5;
+const FooterBg = styled.div`
+    position: absolute;
+    z-index: 0;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 1340px;
+
+    @media (min-width: 1200px) {
+        width: 1920px;
+    }
+
+    .footer-pattern-bg__default {
+        display: block;
+        width: 100%;
+        @media (min-width: 2560px) {
+            display: none;
+        }
+    }
+    .footer-pattern-bg__large {
+        display: none;
+        @media (min-width: 2560px) {
+            display: block;
+            width: 100%;
+        }
+    }
 `;
 
-const StyledFooterSection = styled.section`
-    max-width: 600px;
-    margin: 0 auto 100px;
+const FooterWhiteMask = styled.div`
+    position: absolute;
+    z-index: 1;
+    top: -8px;
+    left: 50%;
+    width: 727px;
+    transform: translateX(-50%);
+
+    @media (min-width: 768px) {
+        top: -14px;
+        width: 1340px;
+    }
+    @media (min-width: 1200px) {
+        top: -20px;
+        width: 1920px;
+    }
+    .footer-white-mask__default {
+        display: block;
+        width: 100%;
+        @media (min-width: 2560px) {
+            display: none;
+        }
+    }
+    .footer-white-mask__large {
+        display: none;
+        @media (min-width: 2560px) {
+            display: block;
+            width: 100%;
+        }
+    }
+`;
+
+const StyledContainerSection = styled.section`
+    position: relative;
+    z-index: 2;
+`;
+
+const StyledContainer = styled.div`
     text-align: center;
-    position: relative;
-    z-index: 10;
-    @media (max-width: 768px) {
-        margin: 0 auto 50px;
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+
+    @media (min-width: 576px) {
+        max-width: calc(100% - 50px);
+    }
+    @media (min-width: 992px) {
+        max-width: calc(100% - 90px);
+    }
+    @media (min-width: 1200px) {
+        max-width: 1140px;
     }
     p {
         color: var(--white);
     }
+
     .title {
         color: var(--white);
         font-size: clamp(40px, 5vw, 60px);
@@ -37,36 +116,51 @@ const StyledFooterSection = styled.section`
 
 const Footer = () => {
     return (
-        <Wrapper>
-            <Image
-                src="/footer-white-mask.png"
-                alt="footer-white-mask"
-                layout="responsive"
-                width={1920}
-                height={369}
-                quality={100}
-            />
-            <StyledFooterSection id="contact">
-                <h2 className="title">Get In Touch</h2>
+        <StyledFooterSection>
+            <FooterBg>
+                <div>
+                    <img
+                        className="footer-pattern-bg__default"
+                        src="/footer-pattern-bg.png"
+                        alt="footer-pattern-bg"
+                    />
+                    <img
+                        className="footer-pattern-bg__large"
+                        src="/footer-pattern-bg-lg.png"
+                        alt="footer-pattern-bg-lg"
+                    />
+                </div>
+            </FooterBg>
 
-                <p>
-                    I&apos;m currently looking for new opportunities, my inbox is always open.
-                    Whether you have a question or just want to say hi, I&apos;ll try my best to get
-                    back to you!
-                </p>
+            <FooterWhiteMask>
+                <img
+                    className="footer-white-mask__default"
+                    src="/footer-white-mask.png"
+                    alt="footer-white-mask"
+                />
+                <img
+                    className="footer-white-mask__large"
+                    src="/footer-pattern-bg-lg.png"
+                    alt="footer-pattern-bg-lg"
+                />
+            </FooterWhiteMask>
 
-                <a className="email-link" href={`mailto:${email}`}>
-                    Say Hello
-                </a>
-            </StyledFooterSection>
-            <BgPattern
-                src="/footer-pattern-bg.png"
-                alt="background-pattern"
-                layout="fill"
-                objectFit="cover"
-                quality={100}
-            />
-        </Wrapper>
+            <StyledContainerSection id="contact">
+                <StyledContainer>
+                    <h2 className="title">Get In Touch</h2>
+
+                    <p>
+                        I&apos;m currently looking for new opportunities, my inbox is always open.
+                        Whether you have a question or just want to say hi, I&apos;ll try my best to
+                        get back to you!
+                    </p>
+
+                    <a className="email-link" href={`mailto:${email}`}>
+                        Say Hello
+                    </a>
+                </StyledContainer>
+            </StyledContainerSection>
+        </StyledFooterSection>
     );
 };
 
